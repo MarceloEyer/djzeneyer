0439f7d500742e5e63764fa286b79be65056d49b
MarceloEyer <eyer.marcelo@Gmail.com>
2025-06-16 21:30:59 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,57 +4,64 @@ import { Routes, Route } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 
 // Layouts
-import MainLayout from './layouts/MainLayout'; // Seu layout principal
+import MainLayout from './layouts/MainLayout';
 
-// Pages (Páginas existentes)
+// Pages
 import HomePage from './pages/HomePage';
-import EventsPage from './pages/EventsPage'; // Corrigido o caminho './ ./pages/EventsPage'
+import EventsPage from './pages/EventsPage';
 import MusicPage from './pages/MusicPage';
-import ZenTribePage from './pages/ZenTribePage'; // Página informativa pública sobre a tribo
-import DashboardPage from './pages/DashboardPage'; // Página para usuários logados
+import ZenTribePage from './pages/ZenTribePage';
+import DashboardPage from './pages/DashboardPage';
 import NotFoundPage from './pages/NotFoundPage';
 
-// Pages (Páginas da Loja)
-import ShopPage from './pages/ShopPage'; // Página principal da loja
-import ProductPage from './pages/ProductPage'; // Página de produto individual
-import CartPage from './pages/CartPage'; // Página do carrinho
-import CheckoutPage from './pages/CheckoutPage'; // Página de finalizar compra
-import MyAccountPage from './pages/MyAccountPage'; // Página da minha conta do usuário
+// Pages (Loja)
+import ShopPage from './pages/ShopPage';
+import ProductPage from './pages/ProductPage';
+import CartPage from './pages/CartPage';
+import CheckoutPage from './pages/CheckoutPage';
+import MyAccountPage from './pages/MyAccountPage';
 
-// Context Providers
-import { MusicPlayerProvider } from './contexts/MusicPlayerContext';
+// Context Providers - TODOS JUNTOS AQUI
+import { LanguageProvider } from './contexts/LanguageContext';
 import { UserProvider } from './contexts/UserContext';
+import { CartProvider } from './contexts/CartContext';
+import { MusicPlayerProvider } from './contexts/MusicPlayerContext';
 
 function App() {
   return (
-    <UserProvider>
-      <MusicPlayerProvider>
-        <AnimatePresence mode="wait">
-          <Routes>
-            <Route path="/" element={<MainLayout />}>
-              {/* Rotas Principais */}
-              <Route index element={<HomePage />} />
-              <Route path="events" element={<EventsPage />} />
-              <Route path="music" element={<MusicPage />} />
-              <Route path="tribe" element={<ZenTribePage />} />
-              {/* ROTA DA DASHBOARD (se você tiver uma página de dashboard separada para usuários) */}
-              {/* Se DashboardPage for a área restrita para assinantes, mantenha esta rota */}
-              <Route path="dashboard" element={<DashboardPage />} /> 
+    // A ordem dos providers é importante. Envolvemos o app com tudo o que é global.
+    <LanguageProvider>
+      <UserProvider>
+        <CartProvider>
+          <MusicPlayerProvider>
+            <AnimatePresence mode="wait">
+              <Routes>
+                <Route path="/" element={<MainLayout />}>
+                  {/* Rotas Principais */}
+                  <Route index element={<HomePage />} />
+                  <Route path="events" element={<EventsPage />} />
+                  <Route path="music" element={<MusicPage />} />
+                  <Route path="tribe" element={<ZenTribePage />} />
+                  
+                  {/* Rota do Dashboard (protegida pelo AdminRoute que configuramos) */}
+                  <Route path="dashboard" element={<DashboardPage />} /> 
 
-              {/* ROTAS DA LOJA */}
-              <Route path="shop" element={<ShopPage />} />
-              <Route path="product/:slug" element={<ProductPage />} />
-              <Route path="cart" element={<CartPage />} />
-              <Route path="checkout" element={<CheckoutPage />} />
-              <Route path="my-account" element={<MyAccountPage />} /> {/* Rota para a área do usuário */}
+                  {/* Rotas da Loja */}
+                  <Route path="shop" element={<ShopPage />} />
+                  <Route path="product/:slug" element={<ProductPage />} />
+                  <Route path="cart" element={<CartPage />} />
+                  <Route path="checkout" element={<CheckoutPage />} />
+                  <Route path="my-account" element={<MyAccountPage />} />
 
-              {/* Rota 404 (sempre por último) */}
-              <Route path="*" element={<NotFoundPage />} />
-            </Route>
-          </Routes>
-        </AnimatePresence>
-      </MusicPlayerProvider>
-    </UserProvider>
+                  {/* Rota 404 (sempre por último) */}
+                  <Route path="*" element={<NotFoundPage />} />
+                </Route>
+              </Routes>
+            </AnimatePresence>
+          </MusicPlayerProvider>
+        </CartProvider>
+      </UserProvider>
+    </LanguageProvider>
   );
 }
 
