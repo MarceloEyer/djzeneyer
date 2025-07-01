95356af0ad71e05f9c68da34291108669530878a
MarceloEyer <eyer.marcelo@Gmail.com>
2025-07-01 03:36:07 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-// src/App.tsx (VERSÃO DE TESTE)
-
+// src/App.tsx
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
-
-// Layouts
 import MainLayout from './layouts/MainLayout';
-
-// Pages
 import HomePage from './pages/HomePage';
 import EventsPage from './pages/EventsPage';
 import MusicPage from './pages/MusicPage';
@@ -15,15 +10,11 @@ import ZenTribePage from './pages/ZenTribePage';
 import DashboardPage from './pages/DashboardPage';
 import NotFoundPage from './pages/NotFoundPage';
 import PressKitPage from './pages/PressKitPage';
-
-// Pages (Loja)
 import ShopPage from './pages/ShopPage';
 import ProductPage from './pages/ProductPage';
 import CartPage from './pages/CartPage';
 import CheckoutPage from './pages/CheckoutPage';
 import MyAccountPage from './pages/MyAccountPage';
-
-// Context Providers
 import { LanguageProvider } from './contexts/LanguageContext';
 import { UserProvider } from './contexts/UserContext';
 import { CartProvider } from './contexts/CartContext';
@@ -38,14 +29,17 @@ function App() {
             <AnimatePresence mode="wait">
               <Routes>
                 <Route path="/" element={<MainLayout />}>
-                  {/* ... outras rotas ... */}
+                  <Route index element={<HomePage />} />
+                  <Route path="events" element={<EventsPage />} />
+                  <Route path="music" element={<MusicPage />} />
                   <Route path="tribe" element={<ZenTribePage />} />
-                  
-                  {/* ALTERAÇÃO DE TESTE ABAIXO */}
-                  <Route path="work-with-me" element={<HomePage />} /> 
-                  
-                  <Route path="dashboard" element={<DashboardPage />} /> 
-                  {/* ... outras rotas ... */}
+                  <Route path="work-with-me" element={<PressKitPage />} />
+                  <Route path="dashboard" element={<DashboardPage />} />
+                  <Route path="shop" element={<ShopPage />} />
+                  <Route path="product/:slug" element={<ProductPage />} />
+                  <Route path="cart" element={<CartPage />} />
+                  <Route path="checkout" element={<CheckoutPage />} />
+                  <Route path="my-account" element={<MyAccountPage />} />
                   <Route path="*" element={<NotFoundPage />} />
                 </Route>
               </Routes>
