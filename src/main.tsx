61ff41f600dcecb0f4120aff4227de835fd7840d
MarceloEyer <eyer.marcelo@Gmail.com>
2025-07-03 13:34:49 -0300

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './index.css'; // CAMINHO CORRIGIDO: Apontando para o arquivo na mesma pasta src
+import './index.css';
 
-import './i18n'; // Inicializa o i18next
+import './i18n'; // Esta linha inicializa tudo
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
