c5f4d8df9bbcad527f8b49d3baaff4cb9228466b
MarceloEyer <eyer.marcelo@Gmail.com>
2025-10-06 02:04:52 -0300

    Updated package.json

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+// src/main.tsx
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './index.css';
+import './index.css'; // CAMINHO CORRIGIDO: Apontando para o arquivo na mesma pasta src
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+import './i18n'; // Inicializa o i18next
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </React.StrictMode>
 );
