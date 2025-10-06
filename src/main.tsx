a2b0ebf3a8d00c5cd73783701e425c9f1462fc16
MarceloEyer <eyer.marcelo@Gmail.com>
2025-10-06 03:17:34 -0300

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+// src/main.tsx
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+// ESTA LINHA "LIGA" O SISTEMA DE TRADUÇÃO PARA TODO O APP
+import './i18n'; 
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </React.StrictMode>
 );
