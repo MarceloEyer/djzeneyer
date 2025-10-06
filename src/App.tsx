3185e971bdd176ca8bf2128c150119d1ff1edad8
MarceloEyer <eyer.marcelo@Gmail.com>
2025-10-06 03:18:46 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 // src/App.tsx
+
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
+import { HelmetProvider } from 'react-helmet-async';
+
+// Layouts e Wrappers
 import MainLayout from './layouts/MainLayout';
+import LanguageWrapper from './components/common/LanguageWrapper';
+
+// Pages
 import HomePage from './pages/HomePage';
 import EventsPage from './pages/EventsPage';
 import MusicPage from './pages/MusicPage';
@@ -15,39 +22,42 @@ import ProductPage from './pages/ProductPage';
 import CartPage from './pages/CartPage';
 import CheckoutPage from './pages/CheckoutPage';
 import MyAccountPage from './pages/MyAccountPage';
-import { LanguageProvider } from './contexts/LanguageContext';
+
+// Context Providers
 import { UserProvider } from './contexts/UserContext';
 import { CartProvider } from './contexts/CartContext';
 import { MusicPlayerProvider } from './contexts/MusicPlayerContext';
 
 function App() {
   return (
-    <LanguageProvider>
+    <HelmetProvider>
       <UserProvider>
         <CartProvider>
           <MusicPlayerProvider>
             <AnimatePresence mode="wait">
               <Routes>
-                <Route path="/" element={<MainLayout />}>
-                  <Route index element={<HomePage />} />
-                  <Route path="events" element={<EventsPage />} />
-                  <Route path="music" element={<MusicPage />} />
-                  <Route path="tribe" element={<ZenTribePage />} />
-                  <Route path="work-with-me" element={<PressKitPage />} />
-                  <Route path="dashboard" element={<DashboardPage />} />
-                  <Route path="shop" element={<ShopPage />} />
-                  <Route path="product/:slug" element={<ProductPage />} />
-                  <Route path="cart" element={<CartPage />} />
-                  <Route path="checkout" element={<CheckoutPage />} />
-                  <Route path="my-account" element={<MyAccountPage />} />
-                  <Route path="*" element={<NotFoundPage />} />
+                <Route path="/:lang(pt)?" element={<LanguageWrapper />}>
+                  <Route element={<MainLayout />}>
+                    <Route index element={<HomePage />} />
+                    <Route path="events" element={<EventsPage />} />
+                    <Route path="music" element={<MusicPage />} />
+                    <Route path="tribe" element={<ZenTribePage />} />
+                    <Route path="work-with-me" element={<PressKitPage />} />
+                    <Route path="dashboard" element={<DashboardPage />} />
+                    <Route path="shop" element={<ShopPage />} />
+                    <Route path="product/:slug" element={<ProductPage />} />
+                    <Route path="cart" element={<CartPage />} />
+                    <Route path="checkout" element={<CheckoutPage />} />
+                    <Route path="my-account" element={<MyAccountPage />} />
+                    <Route path="*" element={<NotFoundPage />} />
+                  </Route>
                 </Route>
               </Routes>
             </AnimatePresence>
           </MusicPlayerProvider>
         </CartProvider>
       </UserProvider>
-    </LanguageProvider>
+    </HelmetProvider>
   );
 }
 
