788435a66708dd9ffe6a26bcb76c549ac59a515b
MarceloEyer <eyer.marcelo@Gmail.com>
2025-10-07 08:19:00 -0300

    Updated config.ts

---
+++ src/config/config.ts
@@ -0,0 +1,22 @@
+// src/config/siteConfig.ts
+
+export const siteConfig = {
+  siteUrl: 'https://djzeneyer.com',
+  defaultTitle: 'DJ Zen Eyer | Brazilian Zouk Music Producer & Live Events',
+  defaultDescription: 'Experience exclusive Brazilian Zouk remixes by DJ Zen Eyer. Join the Zen Tribe for live events, private mixes, and connect with fellow Zouk enthusiasts worldwide.',
+  ogImage: 'https://djzeneyer.com/images/og-image.jpg', // Crie uma imagem de 1200x630 pixels
+  twitterHandle: '@djzeneyer', // Substitua pelo seu @ do Twitter/X
+  twitterImage: 'https://djzeneyer.com/images/twitter-image.jpg', // Crie uma imagem de 1200x600 pixels
+  logo: 'https://djzeneyer.com/images/logo.png', // O caminho para o seu logo
+  socialProfiles: [
+    'https://www.instagram.com/djzeneyer',
+    'https://soundcloud.com/djzeneyer',
+    'https://www.facebook.com/djzeneyer',
+    'https://www.youtube.com/c/djzeneyer', // Exemplo
+  ],
+  // URLs das fontes que estamos usando, para o preload
+  fontPreloads: [
+    'https://fonts.gstatic.com/s/orbitron/v31/yMJRMIlzdpvBhQQL_SCqsYlRFyV1p9YO.woff2',
+    'https://fonts.gstatic.com/s/inter/v13/UcC73FwrK3iLTeHuU_AA6hc.woff2',
+  ],
+};
