42e4ae6ac2e920ecd0dfd544a91fed1cfb0f55c4
MarceloEyer <eyer.marcelo@Gmail.com>
2025-10-07 09:27:18 -0300

    Updated i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,39 +1,23 @@
-// src/i18n.ts
-
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-
-// 1. O caminho agora aponta para dentro de 'src', o que é o correto
 import translationEN from './locales/en/translation.json';
 import translationPT from './locales/pt/translation.json';
 
-// 2. Criamos o objeto de recursos com as traduções importadas
 const resources = {
-  en: {
-    translation: translationEN,
-  },
-  pt: {
-    translation: translationPT,
-  },
+  en: { translation: translationEN },
+  pt: { translation: translationPT },
 };
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources, // 3. Usamos os recursos importados, sem a opção 'backend'
-    fallbackLng: 'en', // Idioma padrão: Inglês americano
+    resources,
+    fallbackLng: 'en',
     supportedLngs: ['en', 'pt'],
-    
-    detection: {
-      order: ['path', 'localStorage', 'cookie'],
-      caches: ['localStorage', 'cookie'],
-    },
-
-    interpolation: {
-      escapeValue: false, 
-    },
+    detection: { order: ['path', 'localStorage', 'cookie'] },
+    interpolation: { escapeValue: false },
   });
 
 export default i18n;
