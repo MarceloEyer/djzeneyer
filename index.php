c73ffd23cf831a6c54983433715fa863e72466a8
MarceloEyer <eyer.marcelo@Gmail.com>
2025-10-07 09:43:35 -0300

    Updated index.php

--- index.php
+++ index.php
@@ -1,18 +1,7 @@
 <?php
 /**
  * Arquivo de template principal.
- * Carrega o header, o root do React, e o footer.
  */
-
-get_header(); ?>
-
-    <main id="root"></main>
-
-    <noscript>
-        <div style="background-color: #1a1a1a; color: white; min-height: 100vh; display: flex; flex-direction: column; align-items: center; justify-content: center; text-align: center; padding: 20px; font-family: sans-serif;">
-            <h1 style="font-size: 24px; margin-bottom: 20px;">JavaScript é Necessário</h1>
-            <p>Para uma experiência completa neste site, é necessário habilitar o JavaScript no seu navegador.</p>
-        </div>
-    </noscript>
-
-<?php get_footer(); ?>
+get_header();
+// O React vai ser montado no #root que está no header.php
+get_footer();
