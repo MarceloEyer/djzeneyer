a28ad453ff4323d6ed44dba3434572b839166a1b
MarceloEyer <eyer.marcelo@Gmail.com>
2025-10-07 10:47:51 -0300

    Updated LanguageWrapper.tsx

--- src/components/common/LanguageWrapper.tsx
+++ src/components/common/LanguageWrapper.tsx
@@ -1,13 +1,13 @@
-// src/components/common/LanguageWrapper.tsx
 import React, { useEffect } from 'react';
 import { useParams, Outlet } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
 const LanguageWrapper: React.FC = () => {
-  const { lang } = useParams<{ lang?: 'pt' }>();
+  const { lang } = useParams<{ lang?: string }>();
   const { i18n } = useTranslation();
 
   useEffect(() => {
+    // Garante fallback para 'en' caso o parâmetro seja ausente/errado
     const targetLang = lang === 'pt' ? 'pt' : 'en';
     if (i18n.language !== targetLang) {
       i18n.changeLanguage(targetLang);
