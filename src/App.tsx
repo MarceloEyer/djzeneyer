bf9d4f00c6a9ac42442baf869cf30490b5b850bc
MarceloEyer <eyer.marcelo@Gmail.com>
2025-10-07 05:47:56 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,25 @@
+// src/App.tsx
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { AnimatePresence } from 'framer-motion';
-import { HelmetProvider } from 'react-helmet-async';
-import MainLayout from './layouts/MainLayout';
+// ... (todos os seus outros imports)
 import LanguageWrapper from './components/common/LanguageWrapper';
+import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
-import MusicPage from './pages/MusicPage';
-import EventsPage from './pages/EventsPage';
-import ShopPage from './pages/ShopPage';
-import ZenTribePage from './pages/ZenTribePage';
-import DashboardPage from './pages/DashboardPage';
-import MyAccountPage from './pages/MyAccountPage';
-import CartPage from './pages/CartPage';
-import CheckoutPage from './pages/CheckoutPage';
-import ProductPage from './pages/ProductPage';
-import PressKitPage from './pages/PressKitPage';
-import NotFoundPage from './pages/NotFoundPage';
-import { UserProvider } from './contexts/UserContext';
-import { CartProvider } from './contexts/CartContext';
-import { MusicPlayerProvider } from './contexts/MusicPlayerContext';
 
 function App() {
   return (
-    <HelmetProvider>
-      <UserProvider>
-        <CartProvider>
-          <MusicPlayerProvider>
-            <AnimatePresence mode="wait">
-              <Routes>
-                <Route path="/:lang(pt)?" element={<LanguageWrapper />}>
-                  <Route element={<MainLayout />}>
-                    <Route index element={<HomePage />} />
-                    <Route path="music" element={<MusicPage />} />
-                    <Route path="events" element={<EventsPage />} />
-                    <Route path="shop" element={<ShopPage />} />
-                    <Route path="product/:id" element={<ProductPage />} />
-                    <Route path="tribe" element={<ZenTribePage />} />
-                    <Route path="dashboard" element={<DashboardPage />} />
-                    <Route path="my-account" element={<MyAccountPage />} />
-                    <Route path="cart" element={<CartPage />} />
-                    <Route path="checkout" element={<CheckoutPage />} />
-                    <Route path="press-kit" element={<PressKitPage />} />
-                    <Route path="*" element={<NotFoundPage />} />
-                  </Route>
-                </Route>
-              </Routes>
-            </AnimatePresence>
-          </MusicPlayerProvider>
-        </CartProvider>
-      </UserProvider>
-    </HelmetProvider>
+    // ... (seus Providers: Helmet, User, Cart, MusicPlayer)
+    <AnimatePresence mode="wait">
+      <Routes>
+        <Route path="/:lang(pt)?" element={<LanguageWrapper />}>
+          <Route element={<MainLayout />}>
+            <Route index element={<HomePage />} />
+            {/* Adicione todas as suas outras rotas aqui */}
+          </Route>
+        </Route>
+      </Routes>
+    </AnimatePresence>
+    // ... (fechamento dos seus Providers)
   );
 }
 export default App;
