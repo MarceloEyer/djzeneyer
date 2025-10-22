06a6019325d9d21a366b6be167047373212ff0a0
MarceloEyer <eyer.marcelo@Gmail.com>
2025-10-22 17:00:01 -0300

    Updated index.ts

---
+++ src/hooks/index.ts
@@ -0,0 +1,13 @@
+// src/hooks/index.ts
+
+export { useGamiPress } from './useGamiPress';
+export { useUserTracks } from './useUserTracks';
+export { useUserEvents } from './useUserEvents';
+export { useUserStreak } from './useUserStreak';
+export { useRecentActivity } from './useRecentActivity';
+
+export type { GamiPressData, Achievement } from './useGamiPress';
+export type { UserTracksData, Track } from './useUserTracks';
+export type { UserEventsData, Event } from './useUserEvents';
+export type { UserStreakData } from './useUserStreak';
+export type { RecentActivityData, Activity } from './useRecentActivity';
