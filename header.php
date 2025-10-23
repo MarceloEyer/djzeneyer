f212158d9356f95ef4489c1e25226a4767bc3ff1
MarceloEyer <eyer.marcelo@Gmail.com>
2025-10-23 07:05:41 -0300

    Updated header.php

--- header.php
+++ header.php
@@ -4,6 +4,8 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg">
+<link rel="apple-touch-icon" sizes="120x120" href="/favicon-120.png">
 </head>
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
