2d6aef80d37800d2993d13e25903c15fc211700d
DJ Zen Eyer <eyer.marcelo@Gmail.com>
2025-11-12 16:25:33 -0300

    Updated cleanup.php

---
+++ inc/cleanup.php
@@ -0,0 +1,21 @@
+<?php
+if (!defined('ABSPATH')) exit;
+
+add_action('after_setup_theme', function () {
+    show_admin_bar(false);
+    remove_action('wp_head', 'print_emoji_detection_script', 7);
+    remove_action('admin_print_scripts', 'print_emoji_detection_script');
+    remove_action('wp_print_styles', 'print_emoji_styles');
+});
+
+add_action('wp_enqueue_scripts', function () {
+    wp_dequeue_style('wp-block-library');
+    wp_dequeue_style('wp-block-library-theme');
+    wp_dequeue_style('wc-blocks-style');
+}, 100);
+
+add_action('init', function() {
+    remove_action('wp_head', 'rsd_link');
+    remove_action('wp_head', 'wlwmanifest_link');
+    remove_action('wp_head', 'wp_generator');
+});
