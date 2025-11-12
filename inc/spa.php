41dc0a4e24317aee6928496bb79b6c587438ea83
DJ Zen Eyer <eyer.marcelo@Gmail.com>
2025-11-12 16:34:14 -0300

    Updated spa.php

--- inc/spa.php
+++ inc/spa.php
@@ -1,16 +1,21 @@
 <?php
 if (!defined('ABSPATH')) exit;
 
-// Roteamento Principal
+/* =========================
+ * ROTEAMENTO SPA
+ * ========================= */
 add_filter('template_include', function ($template) {
     if (is_admin() || (defined('REST_REQUEST') && REST_REQUEST) || !is_main_query() || !is_404()) {
         return $template;
     }
+    
     status_header(200);
     return get_theme_file_path('/index.php');
 });
 
-// Fix para garantir header 404 em arquivos reais inexistentes
+/* =========================
+ * FIX 404
+ * ========================= */
 add_action('template_redirect', function() {
     if (is_404()) {
         status_header(404);
