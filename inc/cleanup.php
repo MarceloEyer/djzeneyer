205532d3a73f89e6aebd0361ee72f7f9470b9bd0
DJ Zen Eyer <eyer.marcelo@Gmail.com>
2025-11-12 16:34:59 -0300

    Updated cleanup.php

--- inc/cleanup.php
+++ inc/cleanup.php
@@ -2,20 +2,34 @@
 if (!defined('ABSPATH')) exit;
 
 add_action('after_setup_theme', function () {
+    // Remove Barra de Admin no Frontend
     show_admin_bar(false);
+    
+    // Remove Emojis do WP
     remove_action('wp_head', 'print_emoji_detection_script', 7);
     remove_action('admin_print_scripts', 'print_emoji_detection_script');
     remove_action('wp_print_styles', 'print_emoji_styles');
+    remove_action('admin_print_styles', 'print_emoji_styles');
+    remove_filter('the_content_feed', 'wp_staticize_emoji');
+    remove_filter('comment_text_rss', 'wp_staticize_emoji');
 });
 
 add_action('wp_enqueue_scripts', function () {
+    // Remove CSS do Gutenberg e Woo
     wp_dequeue_style('wp-block-library');
     wp_dequeue_style('wp-block-library-theme');
     wp_dequeue_style('wc-blocks-style');
+    
+    // Remove CSS padrão do WooCommerce
+    add_filter('woocommerce_enqueue_styles', '__return_false');
 }, 100);
 
 add_action('init', function() {
+    // Limpeza de Cabeçalho
     remove_action('wp_head', 'rsd_link');
     remove_action('wp_head', 'wlwmanifest_link');
     remove_action('wp_head', 'wp_generator');
+    remove_action('wp_head', 'wp_shortlink_wp_head');
+    remove_action('wp_head', 'feed_links', 2);
+    remove_action('wp_head', 'feed_links_extra', 3);
 });
