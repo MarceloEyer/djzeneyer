2302544ddb66aeafd90a5a9c11d9ad387f7179b2
DJ Zen Eyer <eyer.marcelo@Gmail.com>
2025-11-24 07:46:37 -0300

    Added RESUMO_CONFIGURACAO.md

--- inc/setup.php
+++ inc/setup.php
@@ -17,7 +17,9 @@ function djz_allowed_origins(): array {
         'https://www.djzeneyer.com',
         'https://app.djzeneyer.com',
         'http://localhost:5173',
-        'http://127.0.0.1:5173'
+        'http://127.0.0.1:5173',
+        'http://localhost:3000',
+        'http://127.0.0.1:3000'
     ];
 }
 
