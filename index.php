8fb2def7aa4bc579a810b4052852c7fd8658fb97
DJ Zen Eyer <eyer.marcelo@Gmail.com>
2025-12-02 11:27:41 -0300

    Updated index.php

--- index.php
+++ index.php
@@ -99,7 +99,6 @@ $theme_uri = get_template_directory_uri();
     
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700;900&family=Inter:wght@300;400;500;700&display=swap">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700;900&family=Inter:wght@300;400;500;700&display=swap" media="print" onload="this.media='all'">
     <noscript>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700;900&family=Inter:wght@300;400;500;700&display=swap">
