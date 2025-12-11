5014d5ac7e23d3a42fa4c99d636866be54ae3494
DJ Zen Eyer <eyer.marcelo@Gmail.com>
2025-12-11 19:25:24 -0300

    Updated class-zen-bit-api.php

--- plugins/zen-bit/includes/class-zen-bit-api.php
+++ plugins/zen-bit/includes/class-zen-bit-api.php
@@ -12,7 +12,7 @@ class Zen_BIT_API {
     }
     
     private static function get_cache_time() {
-        return (int) get_option('zen_bit_cache_time', 21600); // 6 horas
+        return (int) get_option('zen_bit_cache_time', 80000); // 24 horas
     }
     
     public static function get_events($limit = 50) {
