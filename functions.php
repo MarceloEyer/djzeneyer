a85f8986c36844de483b7fae57a599c82bbaa94e
DJ Zen Eyer <eyer.marcelo@Gmail.com>
2025-12-26 19:15:18 -0300

    Updated functions.php

--- functions.php
+++ functions.php
@@ -19,6 +19,7 @@ require_once get_theme_file_path('/inc/spa.php');        // SPA routing
 require_once get_theme_file_path('/inc/api.php');        // REST endpoints
 require_once get_theme_file_path('/inc/cpt.php');        // Custom post types
 require_once get_theme_file_path('/inc/metaboxes.php');  // Admin metaboxes
+require_once get_theme_file_path('/inc/ai-llm.php');     // AI LLM
 
 /**
  * Disable admin bar on frontend
