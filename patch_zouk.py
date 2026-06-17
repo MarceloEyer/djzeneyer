d16a22e264869b488043ecf17478c54dccffac90
Zen Eyer <eyer.marcelo@Gmail.com>
2026-06-17 05:17:44 -0300

    chore: remove generated patch artifacts

--- patch_zouk.py
+++
@@ -1,9 +0,0 @@
-with open("src/pages/ZoukFestivalsPage.tsx", "r") as f:
-    content = f.read()
-
-content = content.replace("}, [ARTIST.festivals]);", "}, []);")
-
-with open("src/pages/ZoukFestivalsPage.tsx", "w") as f:
-    f.write(content)
-
-print("Patched Zouk")
